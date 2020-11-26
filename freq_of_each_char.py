4f3bdd4e9107b6214b6067916cc804aefeb5c2dc
vanis-19 <70938255+vanis-19@users.noreply.github.com>
2020-11-26 09:14:02 +0530

    Update freq_of_each_char.py

--- freq_of_each_char.py
+++ freq_of_each_char.py
@@ -2,4 +2,12 @@ s='vanisaddala'
 d={}
 for i in s:
     d[i]=d.get(i,0)+1
-print(d)    
+print(d) 
+
+l=[]
+for i in s:
+    if i not in l:
+        l.append(i)
+for i in sorted(l):
+    print('{} occurs {} times.format(i, s.count(i))
+   
